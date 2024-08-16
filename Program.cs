dee6f7f80df2bb2e222bfdfa09c3f01ed111f287
Adahlen1993 <156604329+Adahlen1993@users.noreply.github.com>
2024-08-16 16:43:26 -0500

    added wage calculation

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int amount = 1234;
+int months = 12;
+
+int yearlyWage = CalculateYearlyWage(amount,months);
+
+Console.WriteLine($"Yearly Wage: {yearlyWage} ");
+
+Console.ReadLine();
+
+
+static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
+{
+    if(numberOfMonthsWorked == 12)
+    return monthlyWage * (numberOfMonthsWorked + 1);
+
+    return monthlyWage * numberOfMonthsWorked;
+}
