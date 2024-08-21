20a7661f1456e68da41f266d9c3cf0cb69f4c47c
Adahlen1993 <156604329+Adahlen1993@users.noreply.github.com>
2024-08-21 10:40:38 -0500

    added helper file

--- Program.cs
+++ Program.cs
@@ -1,20 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using PieShop;
+
 Console.WriteLine("Hello, World!");
 
 int amount = 1234;
 int months = 12;
 
-int yearlyWage = CalculateYearlyWage(amount,months);
+int yearlyWage = Utilities.CalculateYearlyWage(amount,months);
 
 Console.WriteLine($"Yearly Wage: {yearlyWage} ");
 
 Console.ReadLine();
 
-
-static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
-{
-    if(numberOfMonthsWorked == 12)
-    return monthlyWage * (numberOfMonthsWorked + 1);
-
-    return monthlyWage * numberOfMonthsWorked;
-}
---
+++ Utilities.cs
@@ -0,0 +1,12 @@
+namespace PieShop
+{
+    internal class Utilities{
+        public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
+{
+    if(numberOfMonthsWorked == 12)
+    return monthlyWage * (numberOfMonthsWorked + 1);
+
+    return monthlyWage * numberOfMonthsWorked;
+}
+    }
+}
