2b63578f1f4f11d75d22e78cc3a77af57904ba57
Adahlen1993 <156604329+Adahlen1993@users.noreply.github.com>
2024-08-28 10:05:35 -0500

    making employee class

---
+++ Employees.cs
@@ -0,0 +1,13 @@
+namespace PieShop
+{
+    internal class Employee{
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int numberOfHoursWorked;
+        public double wage;
+        public double hourlyRate;
+        
+    }
+}
--- Utilities.cs
+++ Utilities.cs
@@ -8,5 +8,18 @@ namespace PieShop
 
     return monthlyWage * numberOfMonthsWorked;
 }
+ public static int CalculateYearlyWageWithOptional(int monthlyWage, int numberOfMonthsWorked, int bonus = 0)
+{
+   Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
+
+    return monthlyWage * numberOfMonthsWorked + bonus;
+}
+
+ public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked, int bonus)
+{
+   Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
+
+    return monthlyWage * numberOfMonthsWorked + bonus;
+}
     }
 }
